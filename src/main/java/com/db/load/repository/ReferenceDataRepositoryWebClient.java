82be69409c43459bea1799f09184118f84def371
Jordi Gómez <jigg@gft.com>
2022-10-27 16:40:20 +0200

    chore: update application.yml

--- src/main/java/com/db/load/repository/ReferenceDataRepositoryWebClient.java
+++ src/main/java/com/db/load/repository/ReferenceDataRepositoryWebClient.java
@@ -3,7 +3,6 @@ package com.db.load.repository;
 import com.db.load.entity.Book;
 import com.db.load.entity.Counterparty;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
